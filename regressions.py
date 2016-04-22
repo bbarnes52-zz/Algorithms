16f8333c4719550fa89bc8268353e1e7a6b98759
Brian <bgb52@cornell.edu>
2016-04-21 18:46:29 -0700

    making it pretty

--- regressions.py
+++ regressions.py
@@ -1,26 +1,80 @@
+import copy
 import math
+import sys
+from enum import Enum
 from math import exp
+from abc import ABCMeta, abstractmethod
 
-def linearRegression(trainingSet, alpha, algorithm, x=None):
-	numFeatures = len(trainingSet[0])-1
-	theta = [1]*numFeatures
-	thetaCopy = theta
-	for c in range(1000000):
-		for j in range(len(theta)):
-			error = 0
-			for i in range(len(trainingSet)):
-				if algorithm == "local":
-					error = error + guessLocal(trainingSet[i], thetaCopy, x)*trainingSet[i][j]
-				elif algorithm == "logistic":
-					error = error + guessLogistic(trainingSet[i], thetaCopy) * trainingSet[i][j]
-				else:
-					error = error + guess(trainingSet[i], thetaCopy)*trainingSet[i][j]
-			update = alpha * error
-			theta[j] = theta[j]-update
-		thetaCopy = theta
-		print theta
-		
+class ErrorCalculationAlgorithm:
+    __metaclass__ = ABCMeta
 
+    @abstractmethod
+    def calculateError(self, trainingExample, weights): pass
+
+class LinearAlgorithm(ErrorCalculationAlgorithm):
+	def calculateError(self, trainingExample, weights):
+		total = 0
+		for i in range(len(weights)):
+			total = total + weights[i] * trainingExample[i]
+		#TODO depends on format of trainingexample...
+		error = total - trainingExample[len(trainingExample)-1]
+		return error
+
+class RegressionTypes(Enum):
+	linear = 1
+	local = 2
+	logistic = 3
+
+class Regression:
+	def __init__(self, algorithm, trainingSet, learningRate):
+		self.setAlgorithm(algorithm)
+		self.setTrainingSet(trainingSet)
+		self.setlearningRate(learningRate)
+		self.m = len(trainingSet)
+		self.n = len(trainingSet[0])
+
+	def setAlgorithm(self, algorithm):
+		self.algorithm = algorithm
+
+	def setTrainingSet(self, trainingSet):
+		self.trainingSet = trainingSet
+
+	def setLearningRate(self, learningRate):
+		self.learningRate = learningRate
+
+	def performRegression(self):
+		weights = self.initializeWeights()
+		#while(TODO: convergence condition):
+		for c in range(100):
+			weightsForThisIteration = copy.deepcopy(weights)
+			for featureIndex in range(self.n):
+				error = 0
+				for i in range(self.m):
+					#TODO Is it possible to remove parameters from method signature of calculateError?
+					error = error + self.algorithm.calculateError(self.trainingSet[i], weightsForThisIteration, self.n) * trainingSet[i][j]
+				weightUpdate = self.trainingRate * error
+				weights[featureIndex] = weights[featureIndex] - weightUpdate
+		return weights
+
+	def initializeWeights(self):
+		#TODO Should this be defined in scope of performRegression()?
+		return [1] * self.n
+
+trainingSet = [
+[1,2,3,250],
+[1,4,10,700],
+[1,3,2,130],
+[1,6,12,900],
+[1,1,1,100],
+[1,4,4,300],
+[1,12,15,1400],
+[1,4,4,350],
+[1,5,5,410],
+]
+
+x = Regression(LinearAlgorithm(), trainingSet, .05)
+
+'''
 def guess(trainingExample, theta):
 	total = 0
 	for i in range(len(theta)):
@@ -91,3 +145,6 @@ for example in logisticTrainingSet:
 	print example[len(example)-1]
 	print "\n"
 """	
+
+'''
+
